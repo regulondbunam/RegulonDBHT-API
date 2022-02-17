16c2e71d653756d11a02aaf91f3fb249f837e6e7
andresloal <andresg.loal.95@gmail.com>
2022-02-17 15:12:08 -0600

    Doc: Added function description for new query

--- src/geneExpression/geneExpression_controller.js
+++ src/geneExpression/geneExpression_controller.js
@@ -42,8 +42,8 @@ import {advancedSearchFilter} from 'mongodb-filter-object-parser'
 class geneExpressionController {
     /** Get all geneExpression Docs that are linked to a specific Dataset
      *  @param {String} datasetId the ID of the Dataset that is linked to the document
-     *  @param {String} limit the number of results to be showed
-     *  @param {String} page the page to be showed; this is used to calculate how many
+     *  @param {Number} limit the number of results to be showed
+     *  @param {Number} page the page to be showed; this is used to calculate how many
      *  documents will be skiped to be showed
      */
     static async getAllGeneExpressionOfDataset(datasetId, limit = 10, page = 0) {
@@ -58,6 +58,13 @@ class geneExpressionController {
         return await GeneExpression.findOne({"_id": _id})
     }
 
+    /** Get all geneExpression documents matching in a advancedSearch query
+     *  @param {String} advancedSearch the string that need to be parsed to valid BSON format 
+     *  for MongoDB Query, must have a format like key[value]
+     *  @param {Number} limit the number of results to be showed
+     *  @param {Number} page the page to be showed; this is used to calculate how many
+     *  documents will be skiped to be showed
+     */
     static async getGeneExpressionFromSearch(advancedSearch, limit = 10, page = 0) {
         let offset = page * limit;
         let filter = advancedSearchFilter(advancedSearch)
